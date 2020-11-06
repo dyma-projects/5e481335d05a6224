6a17ebfd646e8a8c05a5d66770d4ed8eaa47ec9c
anshen <plouisin@gmail.com>
2020-11-06 21:15:28 +0100

    projet3

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1-enfant.component.css']
 })
 export class Exercice1EnfantComponent implements OnInit {
+  @Input() compteur: number = 0;
+  @Output() changeCompteur = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  decrementer(){
+    this.compteur--;
+    this.changeCompteur.emit({value: this.compteur});
+  }
+
+  incrementer(){
+    this.compteur++;
+    this.changeCompteur.emit({value: this.compteur});
   }
 
 }
